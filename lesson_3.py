07440ec547ca42c19cc3a07b2a53c08935644a10
galima_mashkova <C:\Users\Olga\AppData\Roaming\The Bat!>
2023-02-28 17:21:51 +0300

    добавили лекции

--- lesson_3.py
+++ lesson_3.py
@@ -16,10 +16,19 @@
 # Программный код, который мы написали прекрасно справляется с
 # поставленной задачей. Давайте изменим наш код и добавим в него
 # return. НО перед этим давайте вспомним, что делает return:
+
+# def sumNumbers(n):
+#     summa = 0
+#     for i in range(1, n + 1):
+#         summa += i
+#     print(summa)
+
 def sumNumbers(n):
     summa = 0
     for i in range(1, n + 1):
         summa += i
-    return summa
-n = int(input()) # 5
-print(sumNumbers(n)) # 15
+    print(summa)
+
+# n = int(input()) # 5
+# sumNumbers(n) # 15
+
