ad02eae05b43b84200a794f556df01be0692142a
Olga Mashkova <C:\Users\Olga\AppData\Roaming\The Bat!>
2023-03-04 14:28:34 +0300

    добавили семинар 6

--- seminar_5.py
+++ seminar_5.py
@@ -68,17 +68,16 @@
 # number = int(input('Введите число: '))
 # print(pos(number))
 
-def fib(n):
-    if n > 0:
-        if n in [1, 2]:
-            return 1
-        return fib(n - 1) + fib(n - 2)
-    else:
-        if n in [-1]:
-            return 1
-        return fib(n + 2) - fib(n + 1)
-list = []
-for i in range(-10, -1):
-    list.append(fib(i))
-print(list)
-   
+# def fib(n):
+#     if n > 0:
+#         if n in [1, 2]:
+#             return 1
+#         return fib(n - 1) + fib(n - 2)
+#     else:
+#         if n in [-1]:
+#             return 1
+#         return fib(n + 2) - fib(n + 1)
+# list = []
+# for i in range(-10, -1):
+#     list.append(fib(i))
+# print(list)
